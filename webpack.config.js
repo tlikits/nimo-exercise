1d51a123124a487a08857b0663298fbdd996a120
tlikits <thanchanok.likitsinsopon@gmail.com>
2024-11-13 08:01:13 +1100

    feat: setup webpack and mocked lambda handler

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    'get-coin-current-price': './src/handlers/get-coin-current-price.ts',
+    'get-search-history': './src/handlers/get-search-history.ts',
+  },
+  target: 'node',
+  mode: 'production',
+  externals: [
+      '@aws-sdk',
+      'aws-lambda'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'handlers/[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2',
+  },
+};
